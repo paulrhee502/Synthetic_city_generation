7fd6e5328e86f772095bb6b7adf7e09b07814619
Varun Nair <varunnair735@gmail.com>
2019-11-13 17:47:45 -0500

    Added preprocessing functionality to roads, still crosswalk dots to iron

--- data_generation/pre_process_syn.py
+++ data_generation/pre_process_syn.py
@@ -16,9 +16,8 @@ import re
 import time
 
 final_res = (650, 650)
-file_name_list = []
 #needs to have a trailing "/"
-file_path = '/Users/Varun/Documents/CityEngine/Default Workspace/c3/images/austin2.8/raw/'
+file_path = '/Users/Varun/Documents/CityEngine/Default Workspace/c3/images/road_patches/test_env/'
 city_name = 'Austin'
 
 
@@ -31,44 +30,61 @@ def num_2_str(num):
 def reject_patch(patch, threshold=0.1): #input threshold is amount of white in RGB file
     return np.mean(np.sum(patch,axis=-1) == 255*3) > threshold
 
+def binarize(file_path, city_name, mode='buildings'):
+    colTileNames = []
+
+    for file in glob.glob(file_path + "*RGB.jpg"):
+        img = smc.imread(file)
+
+        if mode == 'buildings':
+            img_gt = smc.imread(file.replace('RGB', 'GT'))
+        elif mode == 'roads':
+            img_gt = smc.imread(file.replace('RGB', 'GT2'))
+
+        t_shape = min(img.shape[0], img.shape[1])
+
+        img_GT = np.zeros((t_shape, t_shape))
+        img_RGB = np.zeros((t_shape, t_shape, img.shape[2]))
+
+        file = file.replace(file_path, '')
+        # find the number in the name of file
+        num = re.findall(r"\d+\.?\d*", file)
+
+        print("-"*10+'Now processing '+ file +'-'*10)
+
+        img_gt = img_gt[:t_shape, :t_shape, :]
+        # Binarization
+        for i in range(t_shape):
+            for j in range(t_shape):
+                if mode == 'buildings':
+                    if(img_gt[i, j, 0], img_gt[i, j, 1], img_gt[i, j, 2]) < (35, 35, 35):
+                        img_GT[i, j] = 255
+                    else:
+                        img_GT[i, j] = 0
+                elif mode == 'roads':
+                    if(img_gt[i, j, 0], img_gt[i, j, 1], img_gt[i, j, 2]) > (235, 235, 235):
+                        img_GT[i, j] = 0
+                    else:
+                        img_GT[i, j] = 255
+
+        img_GT = smc.imresize(img_GT, final_res, interp='nearest')
+
+        img_RGB = img[:t_shape, :t_shape, :]
+        img_RGB = smc.imresize(img_RGB, final_res, interp='bilinear')
+
+        if reject_patch(img_RGB): #if less than 90% of patch is city
+            print('Too much of patch is on edge')
+            continue
+        else:
+            smc.imsave(city_name + '_' + num_2_str(num[0]) + '_' + num[1] + '_RGB.tif'
+                       , img_RGB)
+            smc.imsave(city_name + '_' + num_2_str(num[0]) + '_' + num[1] + '_GT.tif'
+                       , img_GT)
+        colTileNames.append(city_name + '_' + num_2_str(num[0]) + '_' + num[1])
+    return colTileNames
+
 start_time = time.time()
-for file in glob.glob(file_path + "*RGB.jpg"):
-    img = smc.imread(file)
-    img_gt = smc.imread(file.replace('RGB', 'GT'))
-    t_shape = min(img.shape[0], img.shape[1])
-
-    img_GT = np.zeros((t_shape, t_shape))
-    img_RGB = np.zeros((t_shape, t_shape, img.shape[2]))
-
-    file = file.replace(file_path, '')
-    # find the number in the name of file
-    num = re.findall(r"\d+\.?\d*", file)
-
-    print("-"*10+'Now processing '+ file +'-'*10)
-
-    img_gt = img_gt[:t_shape, :t_shape, :]
-    # Binarization
-    for i in range(t_shape):
-        for j in range(t_shape):
-            if(img_gt[i, j, 0], img_gt[i, j, 1], img_gt[i, j, 2]) < (35, 35, 35):
-                img_GT[i, j] = 255
-            else:
-                img_GT[i, j] = 0
-    img_GT = smc.imresize(img_GT, final_res, interp='nearest')
-
-    img_RGB = img[:t_shape, :t_shape, :]
-    img_RGB = smc.imresize(img_RGB, final_res, interp='bilinear')
-
-    if reject_patch(img_RGB): #if less than 90% of patch is city
-        print('Too much of patch is on edge')
-        continue
-    else:
-        smc.imsave(city_name + '_' + num_2_str(num[0]) + '_' + num[1] + '_RGB.tif'
-                   , img_RGB)
-        smc.imsave(city_name + '_' + num_2_str(num[0]) + '_' + num[1] + '_GT.tif'
-                   , img_GT)
-    file_name_list.append(city_name + '_' + num_2_str(num[0]) + '_' + num[1])
-
+file_name_list = binarize(file_path, city_name, mode='roads')
 # save as file for collection
 print(file_name_list)
 np.savetxt('colTileNames.txt', np.array(file_name_list), fmt='%s', delimiter='\n')
