8110dc1d600f64f43c2064b1a5e35cc11da20857
Varun Nair <varunnair735@gmail.com>
2019-11-14 18:49:36 -0500

    Changed implementation of function in file and added comments

--- data_generation/pre_process_syn.py
+++ data_generation/pre_process_syn.py
@@ -16,31 +16,33 @@ import time
 import cv2 as cv
 from PIL import Image
 
-final_res = (650, 650)
-#needs to have a trailing "/"
-file_path = '/Users/Varun/Documents/CityEngine/Default Workspace/c3/images/road_patches/test_env2/'
-city_name = 'Austin'
-
-
 def num_2_str(num):
+    '''Eliminates numbers in names (not tile numbers) by changing to letter'''
+
     num_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26']
     c_list = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     num2c = dict(zip(num_list, c_list))
     return ''.join([num2c[e_n] for e_n in num])
 
-def reject_patch(patch, threshold=0.1): #input threshold is amount of white in RGB file
+def reject_patch(patch, threshold=0.1):
+    '''
+    Decides whether to keep a patch based on amount of white in tile
+    @param patch: RGB image to be checked
+    @param threshold: in [0,1], lower is stricter amount of white in RGB
+    '''
     return np.mean(np.sum(patch,axis=-1) == 255*3) > threshold
 
-def binarize(file_path, city_name, mode='buildings'):
+def binarize(src_dir, city_name, imageSize=(650,650), mode='buildings'):
     '''
     Function reads list of tile pairs in directory and creates GTs
-    @param file_path: directory with RGB and GT tiles
+    @param src_dir: directory with RGB and GT tiles
     @param city_name: name of city imagery is for
+    @param imageSize: tuple of tile dimensions
     @param mode: takes either 'buildings' or 'roads' to read in GTs differently
     '''
     colTileNames = []
 
-    for file in glob.glob(file_path + "*RGB.jpg"):
+    for file in glob.glob(src_dir + "*RGB.jpg"):
         img = np.array(Image.open(file))
 
         if mode == 'buildings':
@@ -53,7 +55,7 @@ def binarize(file_path, city_name, mode='buildings'):
         img_GT = np.zeros((t_shape, t_shape))
         img_RGB = np.zeros((t_shape, t_shape, img.shape[2]))
 
-        file = file.replace(file_path, '')
+        file = file.replace(src_dir, '')
         # find the number in the name of file
         num = re.findall(r"\d+\.?\d*", file)
 
@@ -80,11 +82,11 @@ def binarize(file_path, city_name, mode='buildings'):
 
         #img_GT = morphology.binary_closing(img_GT) #from skimage import morphology
 
-        img_GT = np.array(Image.fromarray(img_GT).resize(final_res,
+        img_GT = np.array(Image.fromarray(img_GT).resize(imageSize,
                             resample=Image.NEAREST))
 
         img_RGB = img[:t_shape, :t_shape, :]
-        img_RGB = np.array(Image.fromarray(img_RGB).resize(final_res,
+        img_RGB = np.array(Image.fromarray(img_RGB).resize(imageSize,
                             resample=Image.BILINEAR))
 
         if reject_patch(img_RGB): #if less than 90% of patch is city
@@ -95,6 +97,7 @@ def binarize(file_path, city_name, mode='buildings'):
             img_GT = Image.fromarray(img_GT)
 
             img_RGB.save(city_name + '_' + num_2_str(num[0]) + '_' + num[1] + '_RGB.tif')
+            #different file names based on whether it's roads or buildings
             if mode == 'buildings':
                 img_GT.save(city_name + '_' + num_2_str(num[0]) + '_' + num[1] + '_GT.tif')
             elif mode == 'roads':
@@ -104,8 +107,13 @@ def binarize(file_path, city_name, mode='buildings'):
     return colTileNames
 
 start_time = time.time()
-file_name_list = binarize(file_path, city_name, mode='roads')
-# save as file for collection
+final_res = (650, 650)
+#needs to have a trailing "/"
+dir = '/Users/Varun/Documents/CityEngine/Default Workspace/c3/images/road_patches/test_env2/'
+city = 'Austin'
+
+file_name_list = binarize(dir, city, imageSize = final_res, mode='roads')
+
 print(file_name_list)
 np.savetxt('colTileNames.txt', np.array(file_name_list), fmt='%s', delimiter='\n')
 print('Duration: {}'.format(time.time()-start_time))
